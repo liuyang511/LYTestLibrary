3cdeb50fd394b76b7e224f39a5b6a40c2c194013
liuyang <liuyang@housecenter.cn>
2019-10-29 10:45:59 +0800

    no message

---
+++ LYTestLibrary/LYTestLibrary/LYTestLibrary.h
@@ -0,0 +1,17 @@
+//
+//  LYTestLibrary.h
+//  LYTestLibrary
+//
+//  Created by 非请勿入 on 2019/10/29.
+//  Copyright © 2019 liuyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LYTestLibrary : NSObject
+
+
+/// 库文件描述,输出一段文本
+- (void)testMethed;
+
+@end
